88f397f9c16c866e14be37142126daec7d6f1848
Jon Howell <jonh@jonh.net>
2010-06-30 01:49:40 +0000

    missing file
    git-svn-id: https://svn.circlemud.org/svn/rocket/trunk@423 46170a15-b891-4bf7-af97-fd374ce181e0

---
+++ src/lib/potsticker.c
@@ -0,0 +1,51 @@
+#include "potsticker.h"
+
+void ps_update(PotSticker *ps)
+{
+	int16_t new_val = hal_read_adc(ps->adc_channel);
+	int8_t new_left = (new_val+ps->hysteresis)/ps->detents;
+	int8_t new_right = (new_val-ps->hysteresis)/ps->detents;
+
+	static char dbg_v = '_';
+	if (new_left < ps->last_digital_value)
+	{
+		ps->ifi->func(ps->ifi, ps->back);
+		ps->last_digital_value = new_left;
+		dbg_v = 'L';
+	}
+	else if (new_right > ps->last_digital_value)
+	{
+		ps->ifi->func(ps->ifi, ps->fwd);
+		ps->last_digital_value = new_right;
+		dbg_v = 'R';
+	}
+
+#if DEBUG_BRUTE_DISPLAY
+	char msg[8];
+	int_to_string2(msg, 4, 0, ps->last_digital_value);
+	msg[4] = dbg_v;
+	int_to_string2(&msg[5], 3, 0, new_left);
+	SSBitmap bm[8];
+	ascii_to_bitmap_str(bm, 8, msg);
+	program_board(2, bm);
+#endif // DEBUG_BRUTE_DISPLAY
+
+	schedule_us(100, (Activation*) ps);
+}
+
+void init_potsticker(PotSticker *ps, uint8_t adc_channel, InputInjectorIfc *ifi, uint8_t detents, char fwd, char back)
+{
+	ps->func = (ActivationFunc) ps_update;
+
+	ps->adc_channel = adc_channel;
+	hal_init_adc_channel(ps->adc_channel);
+	ps->ifi = ifi;
+	ps->detents = detents;
+	ps->hysteresis = 1024/detents/4;
+	ps->fwd = fwd;
+	ps->back = back;
+
+	ps->last_digital_value = hal_read_adc(ps->adc_channel) / ps->detents;
+
+	schedule_us(1, (Activation*) ps);
+}
