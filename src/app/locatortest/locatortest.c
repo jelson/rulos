219569fbbb164d04da90b856f0a3bcc95bb60b74
Jeremy Elson <jelson@gmail.com>
2010-07-01 01:33:55 +0000

    set svn ignore
    git-svn-id: https://svn.circlemud.org/svn/rocket/trunk@429 46170a15-b891-4bf7-af97-fd374ce181e0

--- src/app/locatortest/locatortest.c
+++ src/app/locatortest/locatortest.c
@@ -8,14 +8,35 @@ typedef struct {
 	TWIRecvSlot *trs;
 } accelAct_t;
 
+#define ACCEL_ADDR 0b0111000
 
-void readAccel(accelAct_t *aa)
+
+void doAccelRead(void *user_data);
+void completeAccelRead(TWIRecvSlot *trs, uint8_t len);
+
+void prepareAccelRead(accelAct_t *aa)
 {
 	aa->sendBuf[0] = 0x2;
-	hal_twi_send(0b1111000, aa->sendBuf, 1, NULL, NULL);
+	hal_twi_send(ACCEL_ADDR, aa->sendBuf, 1, doAccelRead, aa);
 
 	schedule_us(1000000, (Activation *) aa);
 }
+
+void doAccelRead(void *user_data)
+{
+	accelAct_t *aa = (accelAct_t *) user_data;
+	aa->trs = (TWIRecvSlot *) aa->recvBuf;
+	aa->trs->func = completeAccelRead;
+	aa->trs->capacity = 6;
+	aa->trs->user_data = aa;
+	hal_twi_read(ACCEL_ADDR, aa->trs);
+}
+
+void completeAccelRead(TWIRecvSlot *trs, uint8_t len)
+{
+	// do something with the data
+}
+
 
 
 int main()
@@ -23,11 +44,12 @@ int main()
 	heap_init();
 	util_init();
 	hal_init(bc_audioboard);
-	init_clock(10000, TIMER1);
+	init_clock(50000, TIMER1);
+
+	hal_twi_init(0, NULL);
 
 	accelAct_t aa;
-	aa.f = (ActivationFunc) readAccel;
-	hal_twi_init(0, NULL);
+	aa.f = (ActivationFunc) prepareAccelRead;
 	schedule_now((Activation *) &aa);
 
 	CpumonAct cpumon;
