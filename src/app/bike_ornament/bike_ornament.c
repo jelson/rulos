a45401b9d2aee587f700a742276f73862404fbab
Jeremy Elson <jelson@gmail.com>
2017-12-19 09:05:47 +0000

    final version of bicycle pcb
    git-svn-id: https://svn.circlemud.org/svn/rocket/trunk@857 46170a15-b891-4bf7-af97-fd374ce181e0

--- src/app/bike_ornament/bike_ornament.c
+++ src/app/bike_ornament/bike_ornament.c
@@ -30,16 +30,23 @@
 #define HEADLIGHT GPIO_B2
 #define TAILLIGHT GPIO_B1
 
-#define JIFFY_TIME_US 10000
+#define JIFFY_TIME_US 2500
+
 typedef struct {
+  // wheels
+  uint8_t light_on_l;
+  uint8_t light_on_r;
+  Time wheels_next_move_time;
+  
+  // taillight
   r_bool tail_on;
   Time tail_next_toggle_time;
 } BikeState_t;
 
 #define TAIL_ON_TIME_MS 100
 #define TAIL_OFF_TIME_MS 300
+#define WHEEL_PERIOD_MS 10
 
-#if 0
 static inline void clock()
 {
   gpio_set(LED_DRIVER_CLK);
@@ -59,30 +66,30 @@ static void shift_in_config(BikeState_t* bike)
   gpio_clr(LED_DRIVER_LE);
   gpio_clr(LED_DRIVER_CLK);
 
-#if 0
-  // shift in the real data 
-  gpio_set_or_clr(LED_DRIVER_SDI, duktig->light2_on);
-  clock();
-  gpio_set_or_clr(LED_DRIVER_SDI, duktig->light1_on);
-  clock();
+  // Shift in the data.
+  for (int8_t i = 15; i >= 0; i--) {
+    gpio_set_or_clr(LED_DRIVER_SDI_L, (bike->light_on_l == i));
+    gpio_set_or_clr(LED_DRIVER_SDI_R, (bike->light_on_r == i));
+    clock();
+  }
 
-#endif
-
-  // latch the register into the outputs
+  // Disable output; enable latch
   gpio_set(LED_DRIVER_LE);
+
+  // Enable output
   gpio_clr(LED_DRIVER_LE);
 
-  // tidy up (not actually necessary, but makes logic analyzer output easier to read)
+  // tidy up -- not actually necessary, but makes logic analyzer output easier
+  // to read
   gpio_clr(LED_DRIVER_SDI_L);
   gpio_clr(LED_DRIVER_SDI_R);
 }
-#endif
 
 static void bike_update(BikeState_t* bike) {
   schedule_us(JIFFY_TIME_US, (ActivationFuncPtr)bike_update, bike);
   Time now = clock_time_us();
 
-  // If we've reached the toy's timeout, shut down.
+  // If we've reached the next blink state for the tail light, toggle it
   if (later_than(now, bike->tail_next_toggle_time)) {
     if (bike->tail_on) {
       gpio_clr(TAILLIGHT);
@@ -93,6 +100,14 @@ static void bike_update(BikeState_t* bike) {
       bike->tail_on = true;
       bike->tail_next_toggle_time = now + (TAIL_ON_TIME_MS * (uint32_t)1000);
     }
+  }
+
+  // If we've reached the next update time for the bicycle lights, update them
+  if (later_than(now, bike->wheels_next_move_time)) {
+    bike->light_on_l = (bike->light_on_l + 1) % 16;
+    bike->light_on_r = (bike->light_on_r + 1) % 16;
+    shift_in_config(bike);
+    bike->wheels_next_move_time = now + (WHEEL_PERIOD_MS * (uint32_t)1000);
   }
 }
 
@@ -113,8 +128,12 @@ int main() {
   // init state
   BikeState_t bike;
   memset(&bike, 0, sizeof(bike));
+  Time now = clock_time_us();
+  bike.light_on_l = 0;
+  bike.light_on_r = 0;
+  bike.wheels_next_move_time = now;
   bike.tail_on = false;
-  bike.tail_next_toggle_time = clock_time_us();
+  bike.tail_next_toggle_time = now;
 
   // set up periodic update
   init_clock(JIFFY_TIME_US, TIMER1);
