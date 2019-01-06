c8176fbbfaeba269a33d32f75f72be53d59767ec
Jeremy Elson <jelson@gmail.com>
2019-01-06 20:43:16 +0000

    Add addresses for rocket dongles
    git-svn-id: https://svn.circlemud.org/svn/rocket/trunk@925 46170a15-b891-4bf7-af97-fd374ce181e0

--- src/lib/core/network_ports.h
+++ src/lib/core/network_ports.h
@@ -17,19 +17,28 @@
 #pragma once
 
 // Addresses
-#define ROCKET_ADDR				(0x01)
-#define ROCKET1_ADDR			(0x02)
-#define AUDIO_ADDR				(0x03)
+#define ROCKET_ADDR                     (0x01)
+#define ROCKET1_ADDR                    (0x02)
+#define AUDIO_ADDR                      (0x03)
+
+#define DONGLE0_ADDR                    (0x50)
+#define DONGLE1_ADDR                    (0x51)
+#define DONGLE2_ADDR                    (0x52)
+#define DONGLE3_ADDR                    (0x53)
+#define DONGLE4_ADDR                    (0x54)
+#define DONGLE5_ADDR                    (0x55)
+#define DONGLE6_ADDR                    (0x56)
+#define DONGLE7_ADDR                    (0x57)
 
 // Ports
-#define THRUSTER_PORT			(0x11)
-#define REMOTE_BBUF_PORT		(0x12)
-#define REMOTE_KEYBOARD_PORT	(0x13)
-#define REMOTE_SUBFOCUS_PORT0	(0x14)	/* remote_keyboard protocol */
-#define AUDIO_PORT				(0x15)
-#define SCREENBLANKER_PORT		(0x16)
-#define UARTNETWORKTEST_PORT	(0x17)
-#define STREAMING_AUDIO_PORT	(0x18)
-#define SET_VOLUME_PORT			(0x19)
-#define MUSIC_CONTROL_PORT		(0x20)
-#define MUSIC_DISPLAY_PORT		(0x21)
+#define THRUSTER_PORT                   (0x11)
+#define REMOTE_BBUF_PORT                (0x12)
+#define REMOTE_KEYBOARD_PORT            (0x13)
+#define REMOTE_SUBFOCUS_PORT0           (0x14)  /* remote_keyboard protocol */
+#define AUDIO_PORT                      (0x15)
+#define SCREENBLANKER_PORT              (0x16)
+#define UARTNETWORKTEST_PORT            (0x17)
+#define STREAMING_AUDIO_PORT            (0x18)
+#define SET_VOLUME_PORT                 (0x19)
+#define MUSIC_CONTROL_PORT              (0x20)
+#define MUSIC_DISPLAY_PORT              (0x21)
