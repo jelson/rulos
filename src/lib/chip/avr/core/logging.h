0b046f42ece23b0b79e17b2e1166b6d7273df0f0
Jeremy Elson <jelson@gmail.com>
2019-01-15 02:03:03 +0000

    add missing file
    git-svn-id: https://svn.circlemud.org/svn/rocket/trunk@961 46170a15-b891-4bf7-af97-fd374ce181e0

---
+++ src/lib/chip/avr/core/logging.h
@@ -0,0 +1,41 @@
+/************************************************************************
+ *
+ * This file is part of RulOS, Ravenna Ultra-Low-Altitude Operating
+ * System -- the free, open-source operating system for microcontrollers.
+ *
+ * Written by Jon Howell (jonh@jonh.net) and Jeremy Elson (jelson@gmail.com),
+ * May 2009.
+ *
+ * This operating system is in the public domain.  Copyright is waived.
+ * All source code is completely free to use by anyone for any purpose
+ * with no restrictions whatsoever.
+ *
+ * For more information about the project, see: www.jonh.net/rulos
+ *
+ ************************************************************************/
+
+#pragma once
+
+#include "core/util.h"
+#include "hardware.h"
+
+#ifdef LOG_TO_SERIAL
+
+#define LOG(fmt, ...)                        \
+  do {                                       \
+    static const char fmt_p[] PROGMEM = fmt; \
+    avr_log(fmt_p, ##__VA_ARGS__);           \
+  } while (0)
+
+#else  // LOG_TO_SERIAL
+
+#define LOG(...)
+
+#endif  // LOG_TO_SERIAL
+
+#define assert(x)           \
+  do {                      \
+    if (!(x)) {             \
+      avr_assert(__LINE__); \
+    }                       \
+  } while (0)
