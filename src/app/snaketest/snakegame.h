5b26600afb69e635b7e2efb99aac0b7a5535c97d
Jon Howell <jonh@jonh.net>
2019-01-29 18:57:12 +0000

    stubbed out snake game
    git-svn-id: https://svn.circlemud.org/svn/rocket/trunk@1021 46170a15-b891-4bf7-af97-fd374ce181e0

---
+++ src/app/snaketest/snakegame.h
@@ -0,0 +1,48 @@
+/************************************************************************
+ *
+ * This file is part of RulOS, Ravenna Ultra-Low-Altitude Operating
+ * System -- the free, open-source operating system for microcontrollers.
+ *
+ * Written by Jon Howell (jonh@jonh.net) and Jeremy Elson (jelson@gmail.com),
+ * May 2009.
+ *
+ * This operating system is in the public domain.  Copyright is waived.
+ * All source code is completely free to use by anyone for any purpose
+ * with no restrictions whatsoever.
+ *
+ * For more information about the project, see: www.jonh.net/rulos
+ *
+ ************************************************************************/
+
+#pragma once
+
+#include "periph/audio/audio_client.h"
+#include "periph/rocket/rocket.h"
+#include "periph/rocket/screen4.h"
+
+#define PONG_HEIGHT 4
+#define PONG_SCALE2 6
+#define PONG_SCALE (1 << PONG_SCALE2)
+#define PONG_FREQ2 5
+#define PONG_FREQ (1 << PONG_FREQ2)
+
+typedef struct s_pong_handler {
+  UIEventHandlerFunc func;
+  struct s_pong *pong;
+} PongHandler;
+
+typedef struct s_pong {
+  //	BoardBuffer bbuf[PONG_HEIGHT];
+  //	BoardBuffer *btable[PONG_HEIGHT];
+  //	RectRegion rrect;
+  Screen4 *s4;
+  PongHandler handler;
+  int x, y, dx, dy;  // scaled by PONG_SCALE
+  int paddley[2];
+  int score[2];
+  Time lastScore;
+  r_bool focused;
+  AudioClient *audioClient;
+} Pong;
+
+void pong_init(Pong *pong, Screen4 *s4, AudioClient *audioClient);
---
+++ src/app/snaketest/snaketest.c
@@ -0,0 +1,96 @@
+/************************************************************************
+ *
+ * This file is part of RulOS, Ravenna Ultra-Low-Altitude Operating
+ * System -- the free, open-source operating system for microcontrollers.
+ *
+ * Written by Jon Howell (jonh@jonh.net) and Jeremy Elson (jelson@gmail.com),
+ * May 2009.
+ *
+ * This operating system is in the public domain.  Copyright is waived.
+ * All source code is completely free to use by anyone for any purpose
+ * with no restrictions whatsoever.
+ *
+ * For more information about the project, see: www.jonh.net/rulos
+ *
+ ************************************************************************/
+
+#include <inttypes.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "core/board_defs.h"
+#include "core/clock.h"
+#include "core/cpumon.h"
+#include "core/hal.h"
+#include "core/network.h"
+#include "core/util.h"
+#include "periph/rasters/rasters.h"
+#include "../app/snaketest/snakegame.h" // TODO move into lib
+#include "periph/uart/uart.h"
+#include "periph/audio/audio_client.h"
+#include "periph/7seg_panel/7seg_panel.h"
+#include "periph/input_controller/input_controller.h"
+
+/****************************************************************************/
+
+#if !defined(JOYSTICK_X_CHAN) || !defined(JOYSTICK_Y_CHAN)
+#error "JOYSTICK_X_CHAN and JOYSTICK_Y_CHAN must be defined."
+#include <stophere>
+#endif
+
+#define POTSTICKER_CHANNEL 0
+#define VOLUME_POT_CHANNEL 1
+#define USE_LOCAL_KEYPAD 0
+
+HalUart uart;
+CpumonAct cpumon;
+Pong pong;
+
+typedef struct s_direct_injector {
+  InputInjectorFunc injector_func;
+} direct_injector_t;
+
+void cp_inject(struct s_direct_injector *di, char k) {
+  pong.handler.func((UIEventHandler *)(&pong.handler), k);
+}
+
+int main() {
+  hal_init();
+
+  hal_uart_init(&uart, 38400, true, /* uart_id= */ 0);
+  LOG("Log output running\n");
+
+#if NUM_LOCAL_BOARDS > 0
+  hal_init_rocketpanel();
+#endif
+
+  // start the jiffy clock
+  init_clock(10000, TIMER1);
+
+  // includes slow calibration phase
+  cpumon_init(&cpumon);
+
+  board_buffer_module_init();
+
+  Network network;
+  init_twi_network(&network, 200, ROCKET_ADDR);
+
+  AudioClient audio_client;
+  init_audio_client(&audio_client, &network);
+
+  Screen4 s4;
+  uint8_t board0 = 3;
+  init_screen4(&s4, board0);
+
+  direct_injector_t injector;
+  injector.injector_func = (InputInjectorFunc) cp_inject;
+  InputPollerAct ip;
+  input_poller_init(&ip, (InputInjectorIfc *)&injector);    // TODO pass keystrokes straight to snake
+  pong_init(&pong, &s4, &audio_client);
+
+  pong.handler.func((UIEventHandler *) &pong.handler, uie_focus);  // EWW
+  cpumon_main_loop();
+
+  return 0;
+}
