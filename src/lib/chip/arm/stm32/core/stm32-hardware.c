166feb36f83116a3e7c1666f1f620222307bea2e
Jeremy Elson <jelson@gmail.com>
2019-02-06 16:01:57 +0000

    Add clock initialization code to stm32 to use PLL driven by internal oscillator
    git-svn-id: https://svn.circlemud.org/svn/rocket/trunk@1084 46170a15-b891-4bf7-af97-fd374ce181e0

--- src/lib/chip/arm/stm32/core/stm32-hardware.c
+++ src/lib/chip/arm/stm32/core/stm32-hardware.c
@@ -20,11 +20,65 @@
 
 void _init() {}
 
+/**
+ * @brief  System Clock Configuration
+ *         The system Clock is configured as follow :
+ *            System Clock source            = PLL (HSI)
+ *            SYSCLK(Hz)                     = 64000000
+ *            HCLK(Hz)                       = 64000000
+ *            AHB Prescaler                  = 1
+ *            APB1 Prescaler                 = 2
+ *            APB2 Prescaler                 = 1
+ *            PLLMUL                         = 16
+ *            Flash Latency(WS)              = 2
+ * @param  None
+ * @retval None
+ */
+void SystemClock_Config(void) {
+  RCC_ClkInitTypeDef clkinitstruct = {0};
+  RCC_OscInitTypeDef oscinitstruct = {0};
+
+  /* Configure PLL ------------------------------------------------------*/
+  /* PLL configuration: PLLCLK = (HSI / 2) * PLLMUL = (8 / 2) * 16 = 64 MHz */
+  /* PREDIV1 configuration: PREDIV1CLK = PLLCLK / HSEPredivValue = 64 / 1 = 64
+   * MHz */
+  /* Enable HSI and activate PLL with HSi_DIV2 as source */
+  oscinitstruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  oscinitstruct.HSEState = RCC_HSE_OFF;
+  oscinitstruct.LSEState = RCC_LSE_OFF;
+  oscinitstruct.HSIState = RCC_HSI_ON;
+  oscinitstruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  oscinitstruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  oscinitstruct.PLL.PLLState = RCC_PLL_ON;
+  oscinitstruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
+  oscinitstruct.PLL.PLLMUL = RCC_PLL_MUL16;
+  if (HAL_RCC_OscConfig(&oscinitstruct) != HAL_OK) {
+    /* Initialization Error */
+    __builtin_trap();
+  }
+
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
+     clocks dividers */
+  clkinitstruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK |
+                             RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  clkinitstruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  clkinitstruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  clkinitstruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  clkinitstruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  if (HAL_RCC_ClockConfig(&clkinitstruct, FLASH_LATENCY_2) != HAL_OK) {
+    /* Initialization Error */
+    __builtin_trap();
+  }
+}
+
 void hal_init() {
   // Initialize the STM32 HAL library. (Good thing the name didn't
   // collide...)
   HAL_Init();
 
+  SystemClock_Config();
+  SystemCoreClockUpdate();
+  
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
   __HAL_RCC_GPIOC_CLK_ENABLE();
