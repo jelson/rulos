1a4bec809485f738bde8ca6147a7de41e4bc5e2b
Jon Howell <jonh@jonh.net>
2019-02-28 05:16:26 +0000

    replace public domain license with GPLv3
    git-svn-id: https://svn.circlemud.org/svn/rocket/trunk@1146 46170a15-b891-4bf7-af97-fd374ce181e0

--- src/lib/core/random.c
+++ src/lib/core/random.c
@@ -1,18 +1,19 @@
-/************************************************************************
- *
- * This file is part of RulOS, Ravenna Ultra-Low-Altitude Operating
- * System -- the free, open-source operating system for microcontrollers.
- *
- * Written by Jon Howell (jonh@jonh.net) and Jeremy Elson (jelson@gmail.com),
- * May 2009.
- *
- * This operating system is in the public domain.  Copyright is waived.
- * All source code is completely free to use by anyone for any purpose
- * with no restrictions whatsoever.
- *
- * For more information about the project, see: www.jonh.net/rulos
- *
- ************************************************************************/
+/*
+ * Copyright (C) 2009 Jon Howell (jonh@jonh.net) and Jeremy Elson (jelson@gmail.com).
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #include "core/random.h"
 // prng from http://inglorion.net/software/deadbeef_rand/
