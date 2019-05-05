1a5427f237c87c41083cfadf31e78d683218d27d
Jeremy Elson <jelson@gmail.com>
2019-05-05 13:03:08 -0700

    Attach I2S to SD card files for the audio board test program!! Woooooooo!

--- src/app/audioboard2test/audioboard2test.c
+++ src/app/audioboard2test/audioboard2test.c
@@ -17,17 +17,21 @@
  */
 
 #include "core/rulos.h"
+#include "periph/fatfs/ff.h"
 #include "periph/i2s/i2s.h"
 
 //////////////////////////////////////////////////////////////////////////////
 
 #define SAMPLE_BUF_COUNT 4096
+#define FAT_FILE "sample.raw"
 
 typedef struct {
   int num_buffers_filled;
   uint8_t i2s_storage[I2S_STATE_SIZE(SAMPLE_BUF_COUNT)];
   i2s_t* i2s;
   uint16_t samplebuffer[SAMPLE_BUF_COUNT];
+  FATFS fatfs;
+  FIL fp;
 } AudioState;
 
 static void start_audio(void* user_data) {
@@ -40,12 +44,23 @@ static void start_audio(void* user_data) {
 
 static void fill_buffer_cb(void* user_data, uint16_t* buffer_to_fill) {
   AudioState* as = (AudioState*)user_data;
+#ifdef FAT_FILE
+  UINT bytes_read;
+  int retval =
+      f_read(&as->fp, buffer_to_fill, SAMPLE_BUF_COUNT * 2, &bytes_read);
+  if (retval != FR_OK) {
+    LOG("read error reading from %s: %d", FAT_FILE, retval);
+    __builtin_trap();
+  }
+  i2s_buf_filled(as->i2s, buffer_to_fill, bytes_read / 2);
+#else
   if (++as->num_buffers_filled == 11) {
     i2s_buf_filled(as->i2s, buffer_to_fill, 0);
   } else {
     memcpy(buffer_to_fill, as->samplebuffer, sizeof(as->samplebuffer));
     i2s_buf_filled(as->i2s, buffer_to_fill, SAMPLE_BUF_COUNT);
   }
+#endif
 }
 
 static void audio_done_cb(void* user_data) {
@@ -78,7 +93,20 @@ int main() {
 
   AudioState as;
   memset(&as, 0, sizeof(as));
+
+#ifdef FAT_FILE
+  if (f_mount(&as.fatfs, "", 0) != FR_OK) {
+    LOG("can't mount");
+    __builtin_trap();
+  }
+  if (f_open(&as.fp, FAT_FILE, FA_OPEN_EXISTING | FA_READ) != FR_OK) {
+    LOG("can't open %s", FAT_FILE);
+    __builtin_trap();
+  }
+#else
   init_samples(&as);
+#endif
+
   as.i2s = i2s_init(SAMPLE_BUF_COUNT, 48000, &as, fill_buffer_cb, audio_done_cb,
                     as.i2s_storage, sizeof(as.i2s_storage));
   schedule_now((ActivationFuncPtr)start_audio, &as);
