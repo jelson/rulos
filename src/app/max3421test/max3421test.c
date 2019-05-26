8ccda6dcc7b63ef70178646dad5c10807d70650e
Jeremy Elson <jelson@gmail.com>
2019-05-26 10:11:04 -0700

    Add test driver for max3421e

---
+++ src/app/max3421test/max3421test.c
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2009 Jon Howell (jonh@jonh.net) and Jeremy Elson
+ * (jelson@gmail.com).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "core/rulos.h"
+#include "periph/max3421e/max3421e.h"
+
+#ifdef LOG_TO_SERIAL
+HalUart uart;
+#endif
+
+int main() {
+  hal_init();
+
+#ifdef LOG_TO_SERIAL
+  hal_uart_init(&uart, 115200, true, /* uart_id= */ 0);
+  LOG("Log output running");
+#endif
+
+  init_clock(10000, TIMER1);
+
+  max3421e_t max;
+  max3421e_init(&max);
+
+  cpumon_main_loop();
+}
