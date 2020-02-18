e4ddd285bc0bee68e024853101e1e7e0e165ff81
Jeremy Elson <jelson@gmail.com>
2020-02-17 17:45:43 -0800

    Get audio working again by disabling logging at interrupt time

--- src/lib/periph/i2s/i2s.c
+++ src/lib/periph/i2s/i2s.c
@@ -67,8 +67,10 @@ static void i2s_request_buffer_fill(i2s_t* i2s, uint8_t buf_num) {
 // WARNING: May be called at interrupt time.
 static void i2s_buf_is_playing(i2s_t* i2s, uint8_t play_idx,
                                uint8_t other_idx) {
+#if SIM
   LOG("XXX i2s_buf_is_playing(%d) enters %s,%s", play_idx,
     decode_buf_state[i2s->buf_state[0]], decode_buf_state[i2s->buf_state[1]]);
+#endif
   assert(i2s->buf_state[play_idx] == FULL);
 
   i2s->buf_state[play_idx] = PLAYING;
@@ -78,8 +80,10 @@ static void i2s_buf_is_playing(i2s_t* i2s, uint8_t play_idx,
   if (i2s->samples_in_buf[play_idx] == i2s->samples_per_buf) {
     i2s_request_buffer_fill(i2s, other_idx);
   }
+#if SIM
   LOG("XXX i2s_buf_is_playing exit %s,%s",
     decode_buf_state[i2s->buf_state[0]], decode_buf_state[i2s->buf_state[1]]);
+#endif
 }
 
 // Downcall when a buf fill has been completed by the upper layer.
@@ -209,8 +213,10 @@ static void i2s_buf_played_cb_internal(i2s_t* i2s, uint8_t just_played_idx,
 // WARNING: Called at interrupt time!
 static void i2s_buf_played_cb(void* user_data, uint8_t just_played_idx) {
   i2s_t* i2s = (i2s_t*)user_data;
+#if SIM
   LOG("XXX i2s_buf_played_cb(%d) enters %s,%s", just_played_idx,
     decode_buf_state[i2s->buf_state[0]], decode_buf_state[i2s->buf_state[1]]);
+#endif
 
 #if I2S_STATS
   uint32_t now = precise_clock_time_us();
@@ -227,8 +233,10 @@ static void i2s_buf_played_cb(void* user_data, uint8_t just_played_idx) {
   } else {
     assert(FALSE);
   }
+#if SIM
   LOG("XXX i2s_buf_played_cb exit %s,%s",
     decode_buf_state[i2s->buf_state[0]], decode_buf_state[i2s->buf_state[1]]);
+#endif
 }
 
 //// API implementation
