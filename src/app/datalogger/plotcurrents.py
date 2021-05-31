08817ba83604809aac7dcdf8a26db7f9742a1d80
Jeremy Elson <jelson@gmail.com>
2021-05-30 18:14:22 -0700

    90 sec running average in current plot

--- src/app/datalogger/plotcurrents.py
+++ src/app/datalogger/plotcurrents.py
@@ -6,8 +6,9 @@ import sys
 df = pd.read_csv(sys.argv[1])
 df['time_sec'] = df.iloc[:, 0] / 1000
 df['current_ma'] = df.iloc[:, 1] / 1000
+df['curr_ma_90secroll'] = df['current_ma'].rolling(90).mean()
 print(df)
-plot = df.plot(x='time_sec', y='current_ma', grid=True, figsize=(20, 10))
+plot = df.plot(x='time_sec', y=['current_ma', 'curr_ma_90secroll'], grid=True, figsize=(20, 10))
 plot.set_title('Sony GPS current use')
 plot.set_xlabel('Time (sec)')
 plot.set_ylabel('Current (mA)')
