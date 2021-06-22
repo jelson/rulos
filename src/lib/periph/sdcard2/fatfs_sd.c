d0359dbaef28d9f6335b29e6538c6eedd2982fcb
Jeremy Elson <jelson@gmail.com>
2021-06-21 22:33:46 -0700

    extend SD card write timeout to 250msec

--- src/lib/periph/sdcard2/fatfs_sd.c
+++ src/lib/periph/sdcard2/fatfs_sd.c
@@ -522,7 +522,7 @@ DRESULT TM_FATFS_SD_disk_write (
 				}
 				buff += 512;
 
-				if (!wait_ready(50)) {
+				if (!wait_ready(250)) {
 					LOG("timed out waiting for multiblock ack");
 					goto done;
 				}
@@ -535,7 +535,7 @@ DRESULT TM_FATFS_SD_disk_write (
 	}
 
 	// wait for the write to complete
-	if (!wait_ready(50)) {
+	if (!wait_ready(250)) {
 		LOG("timed out waiting for block ack");
 	}
 
