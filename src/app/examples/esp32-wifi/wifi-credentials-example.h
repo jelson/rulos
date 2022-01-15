ba80c156a72e92783570409524a007e9ad850479
Jeremy Elson <jelson@gmail.com>
2022-01-15 15:44:17 -0800

    Refactor esp32 wifi client into a rulos library

---
+++ src/app/examples/esp32-wifi/wifi-credentials-example.h
@@ -0,0 +1,10 @@
+static const inet_wifi_creds_t wifi_creds[] = {
+    {
+        .ssid = "Jon's House",
+        .password = "Jon's Password",
+    },
+    {
+        .ssid = "Jer's House",
+        .password = "Jer's Password",
+    },
+};
