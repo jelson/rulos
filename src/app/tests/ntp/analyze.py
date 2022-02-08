2fb72e38718c23bace19ebf8d6a63f5ec9adf98f
Jeremy Elson <jelson@gmail.com>
2022-02-08 11:55:42 -0800

    changes to analysis script; check in sample data

--- src/app/tests/ntp/analyze.py
+++ src/app/tests/ntp/analyze.py
@@ -5,6 +5,7 @@ import sys
 import os
 import re
 import numpy as np
+import matplotlib.pyplot as plt
 
 pd.set_option("display.precision", 15)
 
@@ -90,30 +91,86 @@ def gpio_regression_stats(parsed_file):
     df['nearest_sec'] = round(df['exptime_abs'])
     df['error_usec'] = 1000000 * (df['epoch_time'] - df['nearest_sec'])
     df['abserror_usec'] = df['error_usec'].abs()
-    worst = df.nlargest(10, 'abserror_usec')
+    worst = df.nlargest(20, 'abserror_usec')
     print(worst)
 
-    print("abserror:")
-    print(df['abserror_usec'].describe())
-    print(df['abserror_usec'].quantile([0.90, 0.95, 0.99, 0.999]))
+    for offset in [0, 120]:
+        print(f"\nSkipping first {offset} seconds of data:")
+        trimmed = df.loc[df['local'] > offset*1e6]
+        print("abserror:")
+        print(trimmed['abserror_usec'].describe())
+        print(trimmed['abserror_usec'].quantile([0.90, 0.95, 0.99, 0.999]))
 
-    print("signed error:")
-    print(df['error_usec'].describe())
+        print("signed error:")
+        print(trimmed['error_usec'].describe())
 
+    fig, ax = plt.subplots(figsize=(20, 10))
     ax = df.plot(
+        ax=ax,
         x='exptime',
         y=['error_usec'],
         grid=True,
-        figsize=(20, 10))
+    )
     ax.set(
         title='ESP32 time estimate via NTP vs GPS ground truth',
         xlabel='Experiment time (sec)',
         ylabel='Error (usec)'
     )
-    plot_fn = parsed_file.filename + ".gps_vs_ntp.png"
+    plot_fn = parsed_file.filename + ".gps_vs_ntp.timeseries.png"
+    fig.savefig(plot_fn)
+
+    fig, ax = plt.subplots(figsize=(20, 10))
+    ax = df['error_usec'].hist(
+        ax=ax,
+        bins=30,
+        grid=True,
+        rwidth=0.9,
+    )
+    ax.set(
+        title='ESP32 time estimate via NTP vs GPS ground truth',
+        xlabel='Error (usec)',
+    )
+    plot_fn = parsed_file.filename + ".gps_vs_ntp.hist.png"
+    fig.savefig(plot_fn)
+
+def rtt_jitter_stats(parsed_file):
+    df = parsed_file.get_dataframe('stats')
+
+    fig, ax = plt.subplots(figsize=(20, 10))
+    ax = df['raw_rtt_usec'].hist(
+        ax=ax,
+        bins=30,
+        grid=True,
+        rwidth=0.9,
+    )
+    ax.set(
+        title='Distribution of NTP Request RTTs',
+        xlabel='NTP Request RTT (usec)',
+    )
+
+    plot_fn = parsed_file.filename + ".rtt_hist.png"
+    ax.figure.savefig(plot_fn)
+
+def gpio_clockrate_stats(parsed_file):
+    df = parsed_file.get_dataframe('stats')
+    df['ppm'] = df['ppb'] / 1000
+
+    ax = df.plot(
+        x='exptime',
+        y=['ppm'],
+        grid=True,
+        figsize=(20, 10))
+    ax.set(
+        title='ESP32 online clock rate estimate using NTP data',
+        xlabel='Experiment time (sec)',
+        ylabel='Clock Rate Error Estimate (parts per million)'
+    )
+    plot_fn = parsed_file.filename + ".clock_rate_error.png"
     ax.figure.savefig(plot_fn)
 
 if __name__ == "__main__":
     parsed_file = ParsedFile(sys.argv[1])
     #emit_ntp_stats(sys.argv[1])
+    rtt_jitter_stats(parsed_file)
     gpio_regression_stats(parsed_file)
+    gpio_clockrate_stats(parsed_file)
