d7755eb18d8e59a7c20ec6200e1f749550b55030
Dave Jacobowitz <david.jacobowitz@gmail.com>
2022-02-23 21:45:46 -0800

    remove unneeded imports

--- .st_config.py
+++ .st_config.py
@@ -8,13 +8,10 @@
 #     1. pull that repo somewhere convenient
 #     2. add that repo to your path or add an alias to point write to the script st.py
 
-import os, sys
-
-import stlib.commands.shell
-import stlib.commands.pb_decode
-import stlib.shell
-
 CONFIG = {
+    'subpaths': {
+        'rr':   ( 'repo root',  []),
+    },
     'code_search_paths': {
         'rulos': {
             'lib': {
