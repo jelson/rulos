9eedd1bac3970c7a1158cf6aadb04c5df229b45c
Jeremy Elson <jelson@gmail.com>
2022-10-22 23:08:38 -0700

    duktig programmer: enable target power

--- src/app/duktig/program.sh
+++ src/app/duktig/program.sh
@@ -2,9 +2,9 @@
     ~/projects/rulos/build/duktig-v2/arm-stm32g030x6/duktig-v2.elf \
     -ex "set confirm off" \
     -ex "tar ext /dev/ttyACM0" \
+    -ex "mon tpwr ena" \
     -ex "mon swd" \
     -ex "at 1" \
     -ex "load" \
     -ex "mon hard" \
     -ex "quit"
-
