e529572e2788dc88b946e34293c7a74b0a76c1e9
Andrew Marles <andrew.marles@gmail.com>
2024-07-24 10:31:31 -0700

    Initial current calibration values.

--- src/app/datalogger/lora-logger.c
+++ src/app/datalogger/lora-logger.c
@@ -28,19 +28,29 @@
 #define CONSOLE_UART_NUM 0
 #define PSOC_TX_UART_NUM 1
 
+//Outputs
 #define GREEN_LED  GPIO_B1
 #define ORANGE_LED GPIO_B2
+#define RANGE_SEL  GPIO_B0
+#define CHRG_SHDWN GPIO_B9
+#define CLK_DIR    GPIO_A15
+#define DIO_DIR    GPIO_C6
+#define U5TX_DIR   GPIO_B5
+#define U5RX_DIR   GPIO_B8
+
+//Inputs
 #define BOOT_PIN   GPIO_A14
+#define LOG_PIN    GPIO_A0
 
 // power measurement
-#define GPS_POWERMEASURE_ADDR   0b1000000
-#define MODEM_POWERMEASURE_ADDR 0b1000001
+#define BAT_POWERMEASURE_ADDR   0b1000001
+#define MODEM_POWERMEASURE_ADDR 0b1000000
 
 UartState_t console;
 flash_dumper_t flash_dumper;
 serial_reader_t psoc_console_tx;
 
-currmeas_state_t gps_cms, modem_cms;
+currmeas_state_t bat_cms, modem_cms;
 
 void JumpToBootloader (void)
 {
@@ -114,10 +124,35 @@ int main() {
   rulos_hal_init();
 
   gpio_make_input_enable_pullup(BOOT_PIN);
+  gpio_make_input_enable_pullup(LOG_PIN);
 
-  if(gpio_is_clr(BOOT_PIN))
-    JumpToBootloader();
-  
+  gpio_make_output(ORANGE_LED);
+  gpio_make_output(GREEN_LED);
+
+  gpio_make_output(RANGE_SEL);
+  gpio_set(RANGE_SEL);
+  gpio_make_output(CHRG_SHDWN);
+  gpio_set(CHRG_SHDWN);
+
+  //set all translators to input
+  gpio_make_output(CLK_DIR);
+  gpio_clr(CLK_DIR);
+  gpio_make_output(DIO_DIR);
+  gpio_clr(DIO_DIR);
+  gpio_make_output(U5TX_DIR);
+  gpio_clr(U5TX_DIR);
+  gpio_make_output(U5RX_DIR);
+  gpio_clr(U5RX_DIR);  
+
+  //check the flash option.  If it is not clear, fallback to manually reading the pin and jumping.
+  if ( (FLASH->OPTR & FLASH_OPTR_nBOOT_SEL) != 0 ){
+    gpio_set(GREEN_LED);
+    if(gpio_is_clr(BOOT_PIN))
+      JumpToBootloader();
+  }
+  else {
+    gpio_set(ORANGE_LED);
+  }
 
   init_clock(10000, TIMER1);
 
@@ -136,22 +171,30 @@ int main() {
   // initialize current measurement. docs say calibration register should be
   // trunc[0.04096 / (current_lsb * R_shunt)]
 
-  // GPS: R_shunt is 150 milliohms, Iresolution = 66.7
-  // .04096 / (66.7 uA * 0.150 ohms) = 4094
-  currmeas_init(&gps_cms, GPS_POWERMEASURE_ADDR, VOLT_PRESCALE_DIV1, 54613, 5,
-                1, &flash_dumper);
+  //check logger button, set sleep measurement if held at startup.
+  if(gpio_is_clr(LOG_PIN)) {
+    gpio_clr(RANGE_SEL);
+    // BAT: R_shunt is 100R, Iresolution = 0.1
+    // .04096 / (0.1 * 100 ohms) = .004096 * 10K
+    currmeas_init(&bat_cms, BAT_POWERMEASURE_ADDR, VOLT_PRESCALE_DIV1, 45, //calibrated with sourcemeter.
+                  10,
+                  1, &flash_dumper);
+  } else {
+    // BAT: R_shunt is 150 milliohms, Iresolution = 66.7
+    // .04096 / (66.7 uA * 0.150 ohms) = 4.09395 x 10K 
+    currmeas_init(&bat_cms, BAT_POWERMEASURE_ADDR, VOLT_PRESCALE_DIV1, 40940, 
+                  4, //seems best approximation
+                  1, &flash_dumper);
+  }
 
-  // LTE: Rshunt = 0.50 ohms, Iresolution = 10uA
-  // .04096 / (10 uA * 0.5 ohms) = 8192
-  currmeas_init(&modem_cms, MODEM_POWERMEASURE_ADDR, VOLT_PRESCALE_DIV1, 8192,
-                10,  // we read in 10 microamps
+  // LORA: Rshunt = 2 ohms, Iresolution = 5uA
+  // .04096 / (.005 A * 2 ohms) = 4.096 x 10K
+  currmeas_init(&modem_cms, MODEM_POWERMEASURE_ADDR, VOLT_PRESCALE_DIV1, 40960,
+                5,  
                 2, &flash_dumper);                     
 
   // enable periodic blink to indicate liveness
   schedule_now(indicate_alive, NULL);
-
-  gpio_make_output(ORANGE_LED);
-  gpio_make_output(GREEN_LED);
 
   scheduler_run();
   return 0;
